e16bc969b03f083177d2373340eaeaa760bb703b
Slinkywolf <uzulnieksedvarts@gmail.com>
2025-05-07 14:34:37 +0300

    Update Tabs.cs

--- Incremental Game go brrrrrrrrrrrrr/Assets/Scripts/Tabs.cs
+++ Incremental Game go brrrrrrrrrrrrr/Assets/Scripts/Tabs.cs
@@ -136,7 +136,16 @@ public class Tabs : MonoBehaviour
 
     private void EfficientTabChange(object group)
     {
-        CanvasGroup [] groups = new CanvasGroup[120] { hydrogenScreen, heliumScreen, menu, li, be, b, c, n, o, f, ne, na, mg, al, si, p, s, cl, ar, k, ca, sc, ti, v, cr, mn, fe, co, ni, cu, zn, ga, ge, ars, se, br, kr, rb, sr, y, zr, nb, mo, tc, ru, rh, pd, ag, cd, ind, sn, sb, te, i, xe, cs, ba, la, ce, pr, nd, pm, sm, eu, gd, tb, dy, ho, er, tm, yb, lu, hf, ta, w, re, os, ir, pt, au, hg, tl, pb, bi, po, at, rn, fr, ra, ac, th, pa, u, np, pu, am, cm, bk, cf, es, fm, md, no, lr, rf, db, sg, bh, hs, mt, ds, rg, cn, nh, fl, mc, lv, ts, og, changelog};
+        CanvasGroup [] groups = new CanvasGroup[120] 
+		{ 
+		hydrogenScreen, heliumScreen, menu, li, be, b, c, n, o, f, ne, na, mg, 
+		al, si, p, s, cl, ar, k, ca, sc, ti, v, cr, mn, fe, co, ni, cu, zn, ga, 
+		ge, ars, se, br, kr, rb, sr, y, zr, nb, mo, tc, ru, rh, pd, ag, cd, ind, 
+		sn, sb, te, i, xe, cs, ba, la, ce, pr, nd, pm, sm, eu, gd, tb, dy, ho, er, 
+		tm, yb, lu, hf, ta, w, re, os, ir, pt, au, hg, tl, pb, bi, po, at, rn, fr, ra, 
+		ac, th, pa, u, np, pu, am, cm, bk, cf, es, fm, md, no, lr, rf, db, sg, bh, hs, mt, 
+		ds, rg, cn, nh, fl, mc, lv, ts, og, changelog
+		};
     
         foreach (CanvasGroup g in groups)
         {
