c76dd8f5fcda2efc3ec95938b3436b1fae9d9a41
Slinkywolf <uzulnieksedvarts@gmail.com>
2025-05-07 18:50:06 +0300

    Update Classes.cs

--- Incremental Game go brrrrrrrrrrrrr/Assets/Scripts/Classes.cs
+++ Incremental Game go brrrrrrrrrrrrr/Assets/Scripts/Classes.cs
@@ -11,55 +11,225 @@ namespace BreakInfinity
         public BigDouble HydrogenGen = 0;
         public BigDouble PercentageBoost = 0;
     }
-
     public class Helium : MonoBehaviour
     {
         public BigDouble HeliumGain = 0;
         public BigDouble HeliumBoostPower = 0;
         public bool DoubleHelium = false;
     }
-
     public class Lithium : MonoBehaviour
     {
         
     }
-
     public class Beryllium : MonoBehaviour
     {
         
     }
-
     public class Boron : MonoBehaviour
     {
         
     }
-
     public class Carbon : MonoBehaviour
     {
         
     }
-
     public class Nitrogen : MonoBehaviour
     {
         
     }
-
     public class Oxygen : MonoBehaviour
     {
         
     }
-
     public class Fluorine : MonoBehaviour
     {
         
     }
-
     public class Neon : MonoBehaviour
     {
         
     }
-
     public class Sodium : MonoBehaviour
+    {
+        
+    }
+    public class Magnesium : MonoBehaviour
+    {
+        
+    }
+    public class Aluminum : MonoBehaviour
+    {
+        
+    }
+    public class Silicon : MonoBehaviour
+    {
+        
+    }
+    public class Phosphorus : MonoBehaviour
+    {
+        
+    }
+    public class Sulfur : MonoBehaviour
+    {
+        
+    }
+    public class Chlorine : MonoBehaviour
+    {
+        
+    }
+    public class Argon : MonoBehaviour
+    {
+        
+    }
+    public class Potassium : MonoBehaviour
+    {
+        
+    }
+    public class Calcium : MonoBehaviour
+    {
+        
+    }
+    public class Scandium : MonoBehaviour
+    {
+        
+    }
+    public class Titanium : MonoBehaviour
+    {
+        
+    }
+    public class Vanadium : MonoBehaviour
+    {
+        
+    }
+    public class Chromium : MonoBehaviour
+    {
+        
+    }
+    public class Manganese : MonoBehaviour
+    {
+        
+    }
+    public class Iron : MonoBehaviour
+    {
+        
+    }
+    public class Cobalt : MonoBehaviour
+    {
+        
+    }
+    public class Nickel : MonoBehaviour
+    {
+        
+    }
+    public class Copper : MonoBehaviour
+    {
+        
+    }
+    public class Zinc : MonoBehaviour
+    {
+        
+    }
+    public class Gallium : MonoBehaviour
+    {
+        
+    }
+    public class Germanium : MonoBehaviour
+    {
+        
+    }
+    public class Arsenic : MonoBehaviour
+    {
+        
+    }
+    public class Selenium : MonoBehaviour
+    {
+        
+    }
+    public class Bromine : MonoBehaviour
+    {
+        
+    }
+    public class Krypton : MonoBehaviour
+    {
+        
+    }
+    public class Rubidium : MonoBehaviour
+    {
+        
+    }
+    public class Strontium : MonoBehaviour
+    {
+        
+    }
+    public class Yttrium : MonoBehaviour
+    {
+        
+    }
+    public class Zirconium : MonoBehaviour
+    {
+        
+    }
+    public class Niobium : MonoBehaviour
+    {
+        
+    }
+    public class Molybdenum : MonoBehaviour
+    {
+        
+    }
+    public class Technetium : MonoBehaviour
+    {
+        
+    }
+    public class Ruthenium : MonoBehaviour
+    {
+        
+    }
+    public class Rhodium : MonoBehaviour
+    {
+        
+    }
+    public class Palladium : MonoBehaviour
+    {
+        
+    }
+    public class Silver : MonoBehaviour
+    {
+        
+    }
+    public class Cadmium : MonoBehaviour
+    {
+        
+    }
+    public class Indium : MonoBehaviour
+    {
+        
+    }
+    public class Tin : MonoBehaviour
+    {
+        
+    }
+    public class Antimony : MonoBehaviour
+    {
+        
+    }
+    public class Tellurium : MonoBehaviour
+    {
+        
+    }
+    public class Iodine : MonoBehaviour
+    {
+        
+    }
+    public class Xenon : MonoBehaviour
+    {
+        
+    }
+    public class Caresium : MonoBehaviour
+    {
+        
+    }
+    public class Barium : MonoBehaviour
     {
         
     }
